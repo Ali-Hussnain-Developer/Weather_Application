6fc354b87f7c3114572d776c0fc15092ea374ec9
Ali Hussnain <ali.hussnain@arrivy.com>
2024-10-17 10:49:54 +0500

    blink icon removed

--- app/src/main/java/com/example/arrivyfirsttask/MyApplication.kt
+++ app/src/main/java/com/example/arrivyfirsttask/MyApplication.kt
@@ -1,6 +1,7 @@
 package com.example.arrivyfirsttask
 
 import android.app.Application
+import com.example.arrivyfirsttask.constants.Constants
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
@@ -9,7 +10,7 @@ class MyApplication : Application() {
         super.onCreate()
         Realm.init(this)
         val config = RealmConfiguration.Builder()
-            .name("Weather.realm")
+            .name(Constants.REALM_DATABASE_NAME)
             .allowWritesOnUiThread(true)
             .allowQueriesOnUiThread(true)
             .schemaVersion(4)
--- app/src/main/java/com/example/arrivyfirsttask/constants/Constants.kt
+++ app/src/main/java/com/example/arrivyfirsttask/constants/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
         const val API_KEY = "f01e80368f05c66b03425d3f08ab1a1c"
+        const val REALM_DATABASE_NAME = "Weather.realm"
     }
 }
